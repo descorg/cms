c935afcdccfa93f3f5dca93556534a75a4ac0e5c
tang_rong7407 <tang_rong7407@163.com>
2022-05-28 22:23:33 +0800

    队列监听

---
+++ demo/src/main/java/org/springcms/event/RabbitEvent.java
@@ -0,0 +1,26 @@
+package org.springcms.event;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Description;
+import org.springframework.stereotype.Component;
+
+@Component
+@Description("监听队列")
+public class RabbitEvent {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "topic.queue.error.integral"),
+            exchange = @Exchange(value = "topic.queue.exchange.error", type = ExchangeTypes.TOPIC),
+            key = "topic.queue.error.update"
+    ))
+    public void listen(String content) {
+        logger.info(content);
+    }
+}
