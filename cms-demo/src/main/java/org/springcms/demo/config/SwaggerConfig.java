2db7853e9b1d165899ede3d571f8429755dc8cb4
tang_rong7407 <tang_rong7407@163.com>
2022-06-04 09:57:04 +0800

    添加token

--- cms-demo/src/main/java/org/springcms/demo/config/SwaggerConfig.java
+++ cms-demo/src/main/java/org/springcms/demo/config/SwaggerConfig.java
@@ -1,15 +1,21 @@
 package org.springcms.demo.config;
 
+import org.springcms.core.jwt.constant.TokenConstant;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ScalarType;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -21,7 +27,8 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.regex("(?!/error.*).*"))
-                .build();
+                .build()
+                .globalRequestParameters(header());
     }
 
     private ApiInfo apiInfo() {
@@ -32,5 +39,16 @@ public class SwaggerConfig {
                 .contact(new Contact("descorg", "", "tang_rong7407@163.com"))
                 .version("2.0")
                 .build();
+    }
+
+    private List<RequestParameter> header() {
+        return java.util.Collections.singletonList(new springfox.documentation.builders.RequestParameterBuilder()
+                // 不能叫Authorization
+                .name(TokenConstant.HEADER)
+                .description(TokenConstant.HEADER)
+                .in(ParameterType.HEADER)
+                .required(true)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .build());
     }
 }
